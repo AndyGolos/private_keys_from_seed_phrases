b7108bc16a48996051792816e61c97fc192d450a
ango0817 <andrei.golosov@netcracker.com>
2023-04-27 06:27:32 +0530

    Add error handling. Refactoring

--- generate_keys.py
+++ generate_keys.py
@@ -14,19 +14,28 @@ worksheet.column_dimensions['B'].width = 80
 
 worksheet.append(['Seed Phrase', 'Private Key'])
 
-seed_phrases = parse_txt("seed_phrases.txt")
+print("Start generating private keys from seed phrases")
+seed_phrases = parse_txt('seed_phrases.txt')
 
 for i, seed_phrase in enumerate(seed_phrases):
-    hd_wallet = BIP44HDWallet(symbol="ETH")
-    wallet = hd_wallet.from_mnemonic(seed_phrase)
-    worksheet.append([seed_phrase, wallet.private_key()])
+    try:
+        hd_wallet = BIP44HDWallet(symbol='ETH')
+        wallet = hd_wallet.from_mnemonic(seed_phrase)
+        worksheet.append([seed_phrase, wallet.private_key()])
+    except Exception as e:
+        print(f'Seed phrase \"{seed_phrase}\" is invalid. {e}')
+        exit()
 
 alignment = Alignment(horizontal='center', vertical='center')
 for row in worksheet.rows:
     for cell in row:
         cell.alignment = alignment
 
-workbook.save(f'private_keys_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx')
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+file_name = f'private_keys_{timestamp}.xlsx'
+workbook.save(file_name)
+
+print(f'Generation finished. File name: \"{file_name}\"')
 
 
 
